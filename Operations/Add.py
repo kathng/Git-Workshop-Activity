dbed8f29fe7663976760adb900d71f1d5e6c3702
kathng <70711091+kathng@users.noreply.github.com>
2024-02-26 16:56:31 -0800

    Update Add.py

--- Operations/Add.py
+++ Operations/Add.py
@@ -1,3 +1,10 @@
+first = int(input("Enter your first operand: "))
+second = int(input("Enter your second operand: "))
+
 def add(operand_1, operand_2):
     # TODO: Calculate the sum of the 2 operands and return it
-    pass
+    result = first + second
+    print("The result is", result)
+    return result
+
+add(first, second)
